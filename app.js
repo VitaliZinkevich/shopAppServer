a86bbf57c48fee760630b37aba5b5cd9399e74f3
VitaliZinkevich <vitalizinkevich@gmail.com>
2018-04-28 00:22:45 +0300

    send response db

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ var app = express();
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise
-mongoose.connect ('mongodb://localhost:27017/loginAppDb')
-.then ((err)=>{console.log ('Mongoose UP')})
+mongoose.connect ('mongodb://localhost:27017/bookingObject')
+.then ((err)=>{
+  //if (err) throw err
+
+  console.log ('Mongoose UP')})
 
 app.listen(3000, ()=>console.log('listen on 3000'));
+
+app.get ('/test', function (req, res) {
+
+res.json ('HI we done')
+
+})
